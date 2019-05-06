64a8dcd7040093d37d8a19eb9bb146c62c82003a
XxCreditIsGoodXx <50156391+XxCreditIsGoodXx@users.noreply.github.com>
2019-05-06 19:27:59 -0400

    JoinSigns is on build

---
+++ src/XxCreditIsGoodXx/MasterBuilders/RefreshSigns.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace XxCreditIsGoodXx;
+
+use pocketmine\tile\Sign;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\level\Level;
+
+class RefreshSigns extends PluginTask {
