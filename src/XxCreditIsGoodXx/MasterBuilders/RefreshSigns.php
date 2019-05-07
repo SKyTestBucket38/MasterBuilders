642cc92bd10e6ff081905d6d4abe09ea2c1b5b8f
XxCreditIsGoodXx <50156391+XxCreditIsGoodXx@users.noreply.github.com>
2019-05-06 20:09:19 -0400

    New JoinSign build

---
+++ src/XxCreditIsGoodXx/MasterBuilders/RefreshSigns.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BuildBattle\events;
+
+use pocketmine\scheduler\PluginTask;
+
+use pocketmine\event\Listener;
+
+use pocketmine\utils\Config;
+
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+
+use pocketmine\math\Vector3;
+
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+
+use pocketmine\block\Block;
+use pocketmine\tile\Sign;
+
+use
